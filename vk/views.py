f19b3f3c2cc9f047f0c6146c466eae341c16cd1a
Vyacheslav <assimilation13cd@gmail.com>
2021-09-07 23:30:09 +0300

    release and test VK_ANSWER

--- vk/views.py
+++ vk/views.py
@@ -9,6 +9,7 @@ from vk.models import Post
 TG_TOKEN = ''
 VK_ANSWER = ''
 NEW_POST = 'wall_post_new'
+CONFIRM = 'confirmation'
 TELEGRAM_URL = f'https://api.telegram.org/bot{TG_TOKEN}/'
 CHAT_ID = ''
 SEND_MESSAGE_TG = TELEGRAM_URL + "SendMessage"
@@ -25,7 +26,9 @@ def send_telegram(post):
 class VkToTg(View):
     def post(self, request):
         vk_message = json.loads(request.body.decode('utf8', errors='ignore'))
-        if vk_message.get('type', False) == NEW_POST:
+        if vk_message.get('type', False) == CONFIRM:
+            return HttpResponse(VK_ANSWER)
+        elif vk_message.get('type', False) == NEW_POST:
             new_post = Post(post_id=vk_message['object'].get('id', ''),
                             owner_id=vk_message['object'].get('owner_id', ''),
                             text=vk_message['object'].get('text', ''))
